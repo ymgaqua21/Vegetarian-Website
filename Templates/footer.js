38ae6076e333abd3a41c03700c7705b50297e11a
Adan <pelon.mm40@gmail.com>
2025-01-18 13:44:20 -0600

    working on the beverages menu

---
+++ Templates/footer.js
@@ -0,0 +1,22 @@
+class Footer extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <div class="footer">
+            <div class="footer-1">
+                <div class="logo">
+                    <img src="/Images/honeybee.png" alt="">
+                </div>
+                <address>
+                    <p>Email: am925953@aldine-isd.org</p>
+                    <p>Adan Mendoza, <br>Street, Houston, Tx </br></p>
+                </address>
+            </div>
+            <div class="footer-2">
+                <h2>Powered by <em>Adan M</em></h2>
+            </div>
+        </div>
+        `;
+    }
+}
+
+customElements.define('main-footer', Footer);
